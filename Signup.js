b7c8b48695caadb8cc321c11b90f37bf6fdfc363
yuva-prashanth <yuvaprashanth20@gmail.com>
2022-04-25 13:20:22 +0530

    Authentic

---
+++ Signup.js
@@ -0,0 +1,38 @@
+import React,{useState} from 'react'
+
+const App = () => {
+  const [data,setData] = useState({
+    username:'',
+    email:'',
+    password:'',
+    confirmPassword:'',
+  })
+  const {username,email,password,confirmPassword } = data;
+  const changeHandler = e => {
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+  const submitHandler = e=>{
+    e.preventDefault();
+    if(password === confirmPassword){
+     console.log(data);
+    }
+    else{
+      console.log('password is not matching');
+    }
+  }
+  return (
+    <div>
+      <center>
+        <form onSubmit={submitHandler} >
+          <input type='text' name='username' value={username} onChange={changeHandler}/><br/>
+          <input type='email'name='email' value={email} onChange={changeHandler}/><br/>
+          <input type='password'name='password' value={password} onChange={changeHandler}/><br/>
+          <input type='password'name='confirmPassword' value={confirmPassword} onChange={changeHandler}/><br/>
+          <input type='submit' name="submit" /><br/>
+        </form>
+      </center>
+    </div>
+  );
+}
+ 
+export default App
